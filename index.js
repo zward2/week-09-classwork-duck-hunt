d0c46d7a02ed21a0e389413e9cd45001733fc266
Ziedah Ward <ziedahward@Ziedahs-MacBook-Pro.local>
2020-07-30 21:28:18 -0400

    Classwork solved

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 //  Hints: What are the basics needed for an express app?
 //         What is missing to configure pug as the view engine
 const scores = require('./scores')
+const express = require ('express')
+
+const app = express()
+
+app.use(express.static("public"))
+app.set('view engine', 'pug')
+
 
 
 // No Changes are needed below this line!!
